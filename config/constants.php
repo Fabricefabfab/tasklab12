80c041c41a8487b47f3114d836edabdcd1cf12d1
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-11-12 00:46:30 +0200

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE_NAME', 'shoppn');
+define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('USER', 'bb389b69eea232');
+define('PASSWORD', 'a60ca44c');
+define('DATABASE_NAME', 'heroku_1267aaff086f44e');
 
 define('CURRENCY', 'USD');
 
